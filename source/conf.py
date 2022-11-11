6290f77adc18bd7e0dffc8d6014a49aa873427fd
Celia789 <96241786+Celia789@users.noreply.github.com>
2022-11-11 15:15:40 +0100

    add theme, markdown, icons

--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,32 @@ author = 'Celia Barat'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+  "github_url": "https://github.com/vferat/",
+  "icon_links": [
+        {
+            "name": "LinkedIn",
+            "url": "https://www.linkedin.com/in/célia-barat-b32436204/,
+            "icon": "fa-brands fa-linkedin",
+        },        
+        ],
+  "search_bar_text": "Search this site...",
+}
